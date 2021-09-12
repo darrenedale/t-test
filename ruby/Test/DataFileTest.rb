cad1ded329a52864ab0531b7ec5b299a6417e5bc
Darren Edale <darren@equituk.net>
2021-09-12 11:41:28 +0100

    Finished DataFile unit test, Ruby implementation
    - added remaining test methods
    - use a test mock for test data instead of writing to a temp file

--- ruby/Test/DataFileTest.rb
+++ ruby/Test/DataFileTest.rb
@@ -3,8 +3,16 @@ require "test/unit";
 
 module Test
     class DataFileTest < Test::Unit::TestCase
-        TestFilePath = "/tmp/TTest-DataFileTest-TestFile-#{Time::now.strftime("%Y%m%d%H%M%S%L")}.csv";
-
+        # Amount by which floating-point tests for equality are allowed to vary.
+        # 
+        # Testing floats for equality is prone to false failures because float representation is inherently imprecise. This
+        # is the maximum amount an actual float value is permitted to vary from its expected value in order to pass
+        # testing.
+        FloatEqualityDelta = 0.000001;
+        
+        # The test data.
+        #
+        # This is the data that appears in the DataFile used for testing.
         TestData = [
             [12.000000, 14.000000,],
             [12.000000, 14.000000,],
@@ -50,56 +58,163 @@ module Test
         # if the data contains identical #s of items in each row/column, how many per row/column
         TestDataUniformRowItemCount = 2;
         TestDataUniformColumnItemCount = 12;
-    
-        def setup()
-            if File.writable?(TestFilePath)
-                File::delete(TestFilePath);
-            end
 
-            print ("Writing test data to #{TestFilePath}\n")
-            outFile = File.open(TestFilePath, "w");
-
-            TestData.each_with_index {
-                | row, rowIndex |
-                if 0 < rowIndex
-                    outFile.write("\n");
-                end
-
-                row.each_with_index {
-                    | value, columnIndex |
-                    if 0 < columnIndex
-                        outFile.write(",");
-                    end
-
-                    outFile.write("%0.6f" % value);
-                }
-            }
-
-            outFile.close();
-        end
-
-        def teardown()
-            if File.writable?(TestFilePath)
-                File::delete(TestFilePath);
-            end
-        end
-
+        ##
+        # Test for Statistics::DataFile.rowCount
         def testRowCount()
             testData = dataFile();
             assert_kind_of(Integer, testData.rowCount, "Row count is not an integer")
             assert_equal(TestDataRowCount, testData.rowCount, "Row count is not correct");
         end
 
+        ##
+        # Test for Statistics::DataFile.rowCount
         def testColumnCount()
             testData = dataFile();
             assert_kind_of(Integer, testData.columnCount, "Column count is not an integer")
             assert_equal(TestDataColumnCount, testData.columnCount, "Column count is not correct");
         end
 
+        ##
+        # Test for Statistics::DataFile.itemCount       
+        def testItemCount()
+            testData = dataFile();
+            assert_kind_of(Integer, testData.itemCount());
+            assert_equal(TestDataItemCount, testData.itemCount());
+        end
+
+        ##
+        # Test for Statistics::DataFile.rowItemCount
+        def testRowItemCount()
+            testData = dataFile();
+            
+            (0 .. TestDataRowCount - 1).each {
+                | row |
+                assert_kind_of(Integer, testData.rowItemCount(row), "Item count for row #{row} is not an integer.");
+                assert_equal(TestDataRowItemCount[row], testData.rowItemCount(row), "Item count for row #{row} is expected to be #{TestDataRowItemCount[row]}");
+            }
+
+            if TestDataHasUniformRows
+                assert_kind_of(Integer, testData.rowItemCount());
+                assert_equal(TestDataUniformRowItemCount, testData.rowItemCount());
+            end
+        end
+
+        ##
+        # Test for Statistics::DataFile.columnItemCount
+        def testColumnItemCount()
+            testData = dataFile();
+            
+            (0 .. TestDataColumnCount - 1).each {
+                | column |
+                assert_kind_of(Integer, testData.columnItemCount(column), "Item count for column #{column} is not an integer.");
+                assert_equal(TestDataColumnItemCount[column], testData.columnItemCount(column), "Item count for column #{column} is expected to be #{TestDataColumnItemCount[column]}");
+            }
+
+
+            if TestDataHasUniformColumns
+                assert_kind_of(Integer, testData.columnItemCount());
+                assert_equal(TestDataUniformColumnItemCount, testData.columnItemCount());
+            end
+        end
+
+        ##
+        # Test for Statistics::DataFile.sum
+        def testSum()
+            testData = dataFile();
+            assert_kind_of(Float, testData.sum());
+            assert_in_delta(TestDataSum, testData.sum(), FloatEqualityDelta);
+            assert_in_delta(TestDataSum, TestDataRowSum.sum(), FloatEqualityDelta, "ERROR IN TEST CODE: expected data file sum and expected row sums do not agree");
+            assert_in_delta(TestDataSum, TestDataColumnSum.sum(), FloatEqualityDelta, "ERROR IN TEST CODE: expected data file sum and expected COLUMN sums do not agree");
+        end
+
+        ##
+        # Test for Statistics::DataFile.rowSum
+        def testRowSum()
+            testData = dataFile();
+            
+            (0 .. TestDataRowCount - 1).each {
+                | row |
+                assert_kind_of(Float, testData.rowSum(row), "Sum for row #{row} is not a floating-point value.");
+                assert_in_delta(TestDataRowSum[row], testData.rowSum(row), FloatEqualityDelta, "Sum for row #{row} is expected to be #{TestDataRowSum[row]}");
+            }
+        end
+
+        ##
+        # Test for Statistics::DataFile.columnSum
+        def testColumnSum()
+            testData = dataFile();
+
+            (0 .. TestDataColumnCount - 1).each {
+            | column |
+                assert_kind_of(Float, testData.columnSum(column), "Sum for column #{column} is not a floating-point value.");
+                assert_in_delta(TestDataColumnSum[column], testData.columnSum(column), FloatEqualityDelta, "Sum for column #{column} is expected to be #{TestDataColumnSum[column]}");
+            }
+        end
+   
+        ##
+        # Test for Statistics::DataFile.mean
+        def testMean()
+            testData = dataFile();
+            assert_kind_of(Float, testData.mean());
+            assert_in_delta(TestDataArithmeticMean, testData.mean(), FloatEqualityDelta);
+        end
+   
+        ##
+        # Test for Statistics::DataFile.rowMean
+        def testRowMean()
+            testData = dataFile();
+
+            (0 .. TestDataRowCount - 1).each {
+            | row |
+                assert_kind_of(Float, testData.rowMean(row), "Mean for row #{row} is not a floating-point value.");
+                assert_in_delta(TestDataRowArithmeticMean[row], testData.rowMean(row), FloatEqualityDelta, "Mean for row #{row} is expected to be #{TestDataRowArithmeticMean[row]}");
+            }
+        end
+   
+        ##
+        # Test for Statistics::DataFile.columnMean
+        def testColumnMean()
+            testData = dataFile();
+
+            (0 .. TestDataColumnCount - 1).each {
+                | column |
+                assert_kind_of(Float, testData.columnMean(column), "Mean for column #{column} is not a floating-point value.");
+                assert_in_delta(TestDataColumnArithmeticMean[column], testData.columnMean(column), FloatEqualityDelta, "Mean for column #{column} is expected to be #{TestDataColumnArithmeticMean[column]}");
+            }
+        end
+   
+        ##
+        # Test for Statistics::DataFile.item
+        def testItem()
+            testData = dataFile();
+
+            (0 .. TestData.length - 1).each {
+                | row |
+                assert_compare(testData.rowCount(), ">", row, "missing row #{row} in data file");
+
+                (0 .. TestData[row].length - 1).each {
+                | column |
+                    assert_compare(testData.columnCount(), ">", column, "missing column in data file");
+                    assert_kind_of(Float, testData.item(row: row, col: column), "Item at R#{row}, C#{column} is expected to be a floating-point value.");
+                    assert_in_delta(TestData[row][column], testData.item(row: row, col: column), FloatEqualityDelta, "Item at R#{row}, C#{column} is expected to be #{TestData[row][column]}");
+                }
+            }
+        end
+
         private
 
+        ##
+        # Fetch a test mock DataFile
+        #
+        # Returns an instance of an anonymous subclass of DataFile where the data is always the test data.
         def dataFile
-            return Statistics::DataFile.new(TestFilePath);
+            return (Class.new(Statistics::DataFile) {
+                def initialize()
+                    super(nil);
+                    @data = TestData;
+                end
+            }).new()
         end
     end
 end
